4a51f40ea3f7c80e74ceeb73dc541b93302b40bb
somayehdodangeh <dodangeh7019@gmail.com>
2023-01-12 03:28:46 -0800

    home

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NoteApp from './components/NoteApp/NoteApp';
+
+function App() {
+  return (
+    <div className="App">
+    <NoteApp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/components/NoteApp/NoteApp.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class NoteApp extends Component {
+  render() {
+    return (
+      <div>
+
+          <h2 class="header upper">A Developer Woman is Loading... </h2>
+          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mx-auto">
+              <input id="input-field" class="form-control" type="text" placeholder="Something here.."/>
+            </div>
+     
+        
+      </div>
+    )
+  }
+}
