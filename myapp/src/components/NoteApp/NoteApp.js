0e84237703905ec9d6abcd56cd6cc3a84ea69de5
somayehdodangeh <dodangeh7019@gmail.com>
2023-01-17 12:52:26 -0800

    add notes

--- myapp/src/components/NoteApp/NoteApp.js
+++ myapp/src/components/NoteApp/NoteApp.js
@@ -26,7 +26,7 @@ export default class NoteApp extends Component {
     inputColor : '#fff'
 
   }
-//    this.addNote = this.addNote.bind(this)
+   this.addNote = this.addNote.bind(this)
    this.noteTitleHandler = this.noteTitleHandler.bind(this)
    this.inputColorHandler = this.inputColorHandler.bind(this)
 //    this.removeNote =  this.removeNote.bind(this)
@@ -46,8 +46,27 @@ export default class NoteApp extends Component {
  }
  emptyInputTitle(){
     this.setState({
-        noteTitle:''
+        noteTitle:'',
+        inputColor : '#fff'
     })
+ }
+
+ addNote(){
+    let newNoteObject = {
+        id: this.state.notes.length + 1,
+        title : this.state.noteTitle,
+        color : this.state.inputColor
+    }
+    console.log(newNoteObject);
+
+    this.setState(prevState => {
+        return {
+            notes :[...prevState .notes, newNoteObject],
+            noteTitle:'',
+            inputColor : '#fff'
+        }
+    })
+
  }
 
 
@@ -76,11 +95,13 @@ export default class NoteApp extends Component {
                                 {this.state.colors.map (color => (
                                   <ColorBox  color={color} key={color} onColor={this.inputColorHandler} />  
                                 ))}
-                                  <ColorBox />
+                                  
                               </div>
                           </div>
                           <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mx-auto my-1 text-right">
-                              <button id="btn-save" type="button" className="btn btn-outline-info"><span className="fa fa-plus" ></span></button>
+                              <button id="btn-save" type="button" className="btn btn-outline-info"
+                              onClick = {this.addNote}>
+                                <span className="fa fa-plus" ></span></button>
                               <button id="btn-delete" type="button" className="btn btn-outline-danger"
                               onClick = {this.emptyInputTitle}
                             >
@@ -96,8 +117,10 @@ export default class NoteApp extends Component {
                           <div className="container">
                               <div className="row">
                                   <div id='listed' className="col-11 col-sm-11 col-md-11 col-lg-11 col-xl-11 p-3 card-columns">
-
-                                      <Note />
+                                   {this.state.notes.map(note =>(
+                                            <Note {...note}  key={note.id}/>
+                                   ))}
+                                    
 
                                   </div>
                               </div>
